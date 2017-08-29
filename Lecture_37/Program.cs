2c3b8faf80873e9b94d3bcb8b08105cb2e7b290c
FabioDsg <fabio.goncalves@catolica.edu.br>
2017-08-29 01:08:46 -0300

    close section 4

---
+++ Lecture_37/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_37
+{
+    public class Person
+    {
+        public int Age;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+
+            var person = new Person() {Age = 20 };
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+                       
+    }
+
+        //Using static to simple calling without a object 
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
+               
+    }
+}
