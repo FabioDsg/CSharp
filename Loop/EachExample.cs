1ec0ce559f16ab504b295dc86868778e9bd72497
FabioDsg <fabio.goncalves@catolica.edu.br>
2017-08-30 17:13:42 -0300

    Iterations

---
+++ Loop/EachExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop
+{
+    class EachExample
+    {
+        public void foreachExample()
+        {
+            // Using foreach
+            var name = "John Smith";
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            // Another example 
+            var numbers = new int[] { 1, 2, 3, 4 };
+
+            foreach (var number in numbers)
+
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ Loop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Uncomment the choice method.
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ex1 = new ForExample();
+            var ex2 = new EachExample();
+
+            //ex1.forExample();
+            //ex2.foreachExample();           
+        }
+    }
+}
