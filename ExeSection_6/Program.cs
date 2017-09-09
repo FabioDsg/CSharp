3cf6ee48c04c04004186997c7c2dcdee9b1879c9
FabioDsg <fabio.goncalves@catolica.edu.br>
2017-09-09 18:24:28 -0300

    End Ex

--- ExeSection_6/Program.cs
+++ ExeSection_6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using CSharp1Exercises.ArraysAndLists;
 
 namespace ExeSection_6
 {
@@ -7,28 +8,14 @@ namespace ExeSection_6
     {
         static void Main(string[] args)
         {
-            var names = new List<string>();
-            while (true)
-            {
-                Console.WriteLine("Type a name or press Enter to exit: ");
-
-                var input = Console.ReadLine();
-                if (String.IsNullOrWhiteSpace(input))
-
-                    break;
-                names.Add(input);
-            }
-
-            if (names.Count > 2)
-                Console.WriteLine("{0}, {1} and {2} others like your post", names[0], names[1], names.Count - 2);
-            else if (names.Count == 2)
-                Console.WriteLine("{0} and {1} like your post", names[0], names[1]);
-            else if (names.Count == 1)
-                Console.WriteLine("{0} likes your post.", names[0]);
-            else
-                Console.WriteLine();
+            Lists list = new Lists();
+            //list.Exercise1();
+            //list.Exercise2();
+            //list.Exercise3();
+            //list.Exercise4();
+            list.Exercise5();
         }
-
+ 
     }
 }    
 
