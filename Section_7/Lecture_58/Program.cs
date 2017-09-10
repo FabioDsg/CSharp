f18934ddf925b72521af6cb769fdb2a64924a487
FabioDsg <fabio.goncalves@catolica.edu.br>
2017-09-10 00:30:30 -0300

    Date

--- Section_7/Lecture_58/Program.cs
+++ Section_7/Lecture_58/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
+
 namespace Lecture_58
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dateTime = new DateTime(2017, 9, 9);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            //Console.WriteLine("Hour: " + now.Hour);
+           //Console.WriteLine("Hour: " + now.Minute);
+
+           var tomorrow = now.AddDays(1);
+           var yesterday = now.AddDays(-1);
+
+           Console.WriteLine(now.ToString("yyyy-MM-dd"));
+
         }
     }
 }
