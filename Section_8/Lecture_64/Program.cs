8e7ad1d501d66c00c0909a5a8f04dc02bbf5a60a
FabioDsg <fabio.goncalves@catolica.edu.br>
2017-09-11 07:22:10 -0300

    Summerize

---
+++ Section_8/Lecture_64/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lecture_64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really long text. ";
+            var summary = StringUtility.SummerizeText(sentence, 25);
+            Console.WriteLine(summary);
+        }
+    }
+}
